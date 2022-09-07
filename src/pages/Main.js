3391df077e84d754ee9359b440d8b811d326ae51
seankim1111 <yongsun199@gmail.com>
2022-09-07 14:53:48 +0900

    style Main page mobile layout

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,22 +3,25 @@ export default function Main() {
     <>
       <div className="bg-Neutrals-Grey9 text-Neutrals-White">
         <div>
-          <section className="py-[4rem] px-4 mx-auto md:max-w-[76rem]">
-            <div className="flex justify-between">
+          <div className="flex md:hidden">
+            <img src="/images/Asset 3@3x 1.png" alt="Asset 3@3x 1" />
+          </div>
+          <section className="md:py-[4rem] pt-8 px-4 mx-auto md:max-w-[76rem]">
+            <div className="md:flex md:justify-between">
               <div>
-                <h1 className="text-[4rem] leading-[4.5rem] font-bold mb-6">
+                <h1 className="md:text-[4rem] text-[2.5rem] md:leading-[4.5rem] leading-[3rem] font-bold mb-6">
                   MORE
-                  <br /> THAN A<br /> PICTURE
+                  <br /> THAN A<br className="hidden md:flex" /> PICTURE
                 </h1>
                 <p className="mb-[3.5rem] H5">
                   8인의 포토저널리스트가 담은
                   <br /> 국경없는의사회 구호현장의 기록
                 </p>
-                <div className="H6 text-Neutrals-Grey5 mb-[6rem]">
+                <div className="H6 text-Neutrals-Grey5 md:mb-[6rem] mb-[4rem]">
                   <p className="mb-2">갤러리 라메르</p>
                   <p>2022.11.02 - 11.14</p>
                 </div>
-                <div className="flex gap-4">
+                <div className="flex gap-4 mb-6 md:mb-0">
                   <img
                     src="/images/svgIcons/content_logo.svg"
                     alt="content_logo"
@@ -27,17 +30,21 @@ export default function Main() {
                   <img src="/images/svgIcons/MAPS_logo.svg" alt="MAPS_logo" />
                 </div>
               </div>
-              <div>
-                <img src="/images/Asset 3@3x 1.png" alt="Asset 3@3x 1" />
+              <div className="hidden md:flex">
+                <img
+                  className="object-cover w-full"
+                  src="/images/Asset 3@3x 1.png"
+                  alt="Asset 3@3x 1"
+                />
               </div>
             </div>
           </section>
-          <section className="bg-bg-blur">
+          <section className="px-4 bg-bg-blur md:px-0">
             <div className="py-[6rem] md:max-w-[76rem] mx-auto">
               <div className="mb-8">
                 <p className="H4">작가 소개</p>
               </div>
-              <div className="grid grid-cols-3 grid-rows-1 gap-8">
+              <div className="hidden grid-cols-3 grid-rows-1 gap-8 md:grid ">
                 <div>
                   <div className="mb-8">
                     <img src="/images/ev_th_1.png" alt="ev_th_1" />
@@ -66,9 +73,20 @@ export default function Main() {
                   </p>
                 </div>
               </div>
+              <div className="grid grid-cols-1 grid-rows-1 gap-8 md:hidden ">
+                <div>
+                  <div className="mb-8">
+                    <img src="/images/ev_th_3.png" alt="ev_th_3" />
+                  </div>
+                  <p className="mb-2 H6">이벤트 내용이 나옵니다.</p>
+                  <p className="text-Neutrals-Grey5 text-[0.875rem] leading-6 font-normal">
+                    2021-11-05
+                  </p>
+                </div>
+              </div>
             </div>
           </section>
-          <section className="bg-Neutrals-Grey8">
+          <section className="px-4 bg-Neutrals-Grey8 md:px-0">
             <div className="py-[6rem] md:max-w-[76rem] mx-auto">
               <div className="flex justify-between mb-8">
                 <p className="H4">이벤트</p>
@@ -77,7 +95,7 @@ export default function Main() {
                   <img src="/images/svgIcons/symbol_+.svg" alt="symbol_+" />
                 </div>
               </div>
-              <div className="grid grid-cols-3 grid-rows-1 gap-8">
+              <div className="hidden grid-cols-3 grid-rows-1 gap-8 md:grid ">
                 <div>
                   <div className="mb-8">
                     <img src="/images/ev_th_1.png" alt="ev_th_1" />
@@ -106,24 +124,35 @@ export default function Main() {
                   </p>
                 </div>
               </div>
+              <div className="grid grid-cols-1 grid-rows-1 gap-8 md:hidden ">
+                <div>
+                  <div className="mb-8">
+                    <img src="/images/ev_th_3.png" alt="ev_th_3" />
+                  </div>
+                  <p className="mb-2 H6">이벤트 내용이 나옵니다.</p>
+                  <p className="text-Neutrals-Grey5 text-[0.875rem] leading-6 font-normal">
+                    2021-11-05
+                  </p>
+                </div>
+              </div>
             </div>
           </section>
           <section className="bg-Brand-Primary">
-            <div className="px-4 md:max-w-[76rem] flex gap-[3.5rem] mx-auto">
-              <div className="flex items-center w-full">
-                국경없는의사회 한국에서 진행하는 이벤트 관련 최신 소식을
-                받아보세요
+            <div className="px-4 md:max-w-[76rem] flex flex-col md:flex-row gap-6 md:gap-[3.5rem] mx-auto py-8 md:py-0">
+              <div className="flex items-center w-full md:min-w-[38.5rem] MH3 md:text-[1.375rem] text-center justify-center md:justify-start">
+                국경없는의사회 한국에서 진행하는
+                <br className="md:hidden" /> 이벤트 관련 최신 소식을 받아보세요
               </div>
-              <div className="flex w-full py-10">
+              <div className="flex flex-col w-full gap-2 py-0 md:py-10 md:flex-row md:gap-0">
                 <div className="w-full">
                   <input
-                    className="w-full px-4 py-3 rounded-l text-Neutrals-Grey9"
+                    className="w-full px-4 py-3 rounded md:rounded-none md:rounded-l text-Neutrals-Grey9"
                     type="text"
                     id="subscribe"
                     placeholder="이메일 주소를 입력해주세요."
                   />
                 </div>
-                <button className="px-10 py-3 rounded-r bg-Neutrals-Grey9 H6 whitespace-nowrap">
+                <button className="px-10 py-3 rounded md:rounded-none md:rounded-r bg-Neutrals-Grey9 H6 whitespace-nowrap">
                   구독하기
                 </button>
               </div>
