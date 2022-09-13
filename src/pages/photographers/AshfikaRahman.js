d39c904368339f06110649275219b7023c288d65
seankim1111 <yongsun199@gmail.com>
2022-09-13 11:16:26 +0900

    add 8번 작가

--- src/pages/photographers/AshfikaRahman.js
+++ src/pages/photographers/AshfikaRahman.js
@@ -5,22 +5,25 @@ export default function AshfikaRahman() {
     <>
       <div className="bg-Neutrals-Grey8 text-Neutrals-White">
         <div className="mb-[4rem] flex justify-center">
-          <img src="/images/GaelTurine/bg.png" alt="bg" />
+          <img src="/images/AshfikaRahman/8 1.png" alt="8 1" />
         </div>
         <div className="bg-Neutrals-Grey8 text-Neutrals-White px-4 mx-auto md:max-w-[76rem]">
           <section className="text-center">
             <div>
-              <p className="H3">가엘 튜린 - 앙골라</p>
-              <p className="mb-6 font-bold H3">Gaël Turine - Angola</p>
-              <p className="mb-4 DB1">쿠이토(Kuito), 1996년 3월 2일</p>
-              <p className="mb-[3.375rem] DB3">Mamiya 6, Tri-X 400</p>
+              <p className="H3">아슈피카 라만 - 방글라데시</p>
+              <p className="mb-6 font-bold H3">Ashfika Rahman - Bangladesh</p>
+              <p className="mb-4 DB1">다카(Dhaka), 2021년 1월 28일 </p>
+              <p className="mb-[3.375rem] DB3">
+                Nikon D810 35mm f 2.8 1/60s ISO 640
+              </p>
               <p className="text-left max-w-[63rem] mx-auto DB1">
-                앙골라 내전 1975~2002 이 장기화하며 극심한 영양실조로 고통받는
-                아동 환자가 급증했다. 심각한 영양실조 위기가 길어지자 폐렴,
-                설사, 말라리라, 홍역 등으로 아동 사망률이 잇따라 증가했다.
-                1996년, 포토저널리스트 튜린은 앙골라에서 6주를 보내며 무장단체간
-                충돌이 끊이지 않는 비에주 쿠이토의 국경없는의사회 병원을 찾아
-                현장을 기록했다.
+                방글라데시 다카 캄란기르차르에 사는 많은 여아는 어린 나이에
+                강제로 결혼해 출산을 겪지만 보건당국은 이들의 고통을 외면하고
+                있다. 전통적인 성역할로 인해 자신의 신체와 관련된 성∙생식 문제에
+                있어 스스로 결정하지 못할 뿐만 아니라 병원을 찾을 경제적 수단이
+                없는 이 소녀들은 심리적∙의료적 지원이 필요하다. 아슈피카 라만은
+                2021년 1월 캄란기르차르에 위치한 국경없는의사회 진료소를 찾아
+                17세 임산부 루키아를 비롯한 소녀들의 모습을 카메라에 담았다.
               </p>
             </div>
             <div className="my-[6rem] h-0 pb-[55.921%] overflow-hidden relative z-0">
@@ -42,62 +45,82 @@ export default function AshfikaRahman() {
               </div>
               <div className="mb-[6rem]">
                 <img
-                  src="/images/GaelTurine/GAEL TURINE 02 1.png"
-                  alt="GAEL TURINE 02 1"
+                  src="/images/AshfikaRahman/ASHIFIKA RAHMAN02 1.png"
+                  alt="ASHIFIKA RAHMAN02 1"
                 />
               </div>
-              <p className="mb-4 max-w-[63rem] mx-auto DB1">
-                “쿠이토에 머물던 중 국경없는의사회와 연락이 닿아 함께 작업할 수
-                있었습니다. 국경없는의사회 직원들의 도움으로 현장 상황을 더욱
-                명확히 파악할 수 있었습니다. 위중한 상태로 병원에 실려온
-                부상자들을 봤고, 견디기 힘든 광경을 마주하기도 했습니다. 다행히
-                크게 무겁지 않은 분위기 속에서 환자와 의료진의 모습을 있는
-                그대로 담을 수 있었습니다.”
-              </p>
-              <p className="max-w-[63rem] mx-auto DB1 font-bold">
-                가엘 튜린(2021)
-              </p>
-              <div className="flex gap-8 my-[6rem]">
+              <div className="flex max-w-[63rem] mx-auto gap-8 mb-[6rem]">
+                <p className="flex-1 my-auto DB1">
+                  “제 이름은 아노와라 베굼입니다. 저는 14살에 결혼해 15살에 첫
+                  아이를 임신했습니다. 더 빨리 임신하게 도와준다는 부적을 받기도
+                  했어요. 게다가 제 어머니는 12살에 첫 아이를 낳았으니 저는 이른
+                  편이 아니었죠. 지금 저는 아들 셋과 딸 하나가 있는데, 딸은 이미
+                  결혼했습니다.”
+                </p>
                 <div>
-                  <img
-                    src="/images/GaelTurine/GAEL TURINE 04 1.png"
-                    alt="GAEL TURINE 04 1"
-                  />
+                  <img src="/images/AshfikaRahman/84.png" alt="84" />
                 </div>
+              </div>
+              <div className="flex max-w-[63rem] mx-auto gap-8 mb-[6rem]">
                 <div>
-                  <img
-                    src="/images/GaelTurine/GAEL TURINE 05 1.png"
-                    alt="GAEL TURINE 05 1"
-                  />
+                  <img src="/images/AshfikaRahman/85.png" alt="85" />
                 </div>
+                <p className="flex-1 my-auto DB1">
+                  사랑하는 사비나,
+                  <br />
+                  <br />
+                  잘 지내고 있으면 좋겠구나. 네 형부는 새 직장을 얻어서 쿨나로
+                  가게 됐어. 네 오빠는 집을 새로 마련하려고 살마한테 돈을 좀
+                  빌리려고 한단다. 다행히 살마가 친정아빠한테 돈을 받아서 줄 수
+                  있다고 해. 고모도 얼마전에 수술을 잘 마쳤어. 네가 임신했다는
+                  소식에 아빠가 매우 좋아하셨단다. 27일에 오빠가 널 집으로
+                  데려온다고 하니 여기서 며칠 쉬다 가렴. 그동안 남편 잘 챙기고,
+                  밥도 잘 챙겨 먹거라.
+                  <br />
+                  <br />
+                  알라신의 축복이 있길,
+                  <br />
+                  엄마가
+                </p>
               </div>
             </div>
           </section>
         </div>
-        <div className="mb-[6rem] flex justify-center">
-          <img
-            src="/images/GaelTurine/GAEL TURINE 06 1.png"
-            alt="GAEL TURINE 06 1"
-          />
+        <div className="mb-[6rem] flex justify-center gap-8">
+          <div>
+            <img
+              src="/images/AshfikaRahman/EXTRA_03_MSF 1.png"
+              alt="EXTRA_03_MSF 1"
+            />
+          </div>
+          <div>
+            <img
+              src="/images/AshfikaRahman/EXTRA_04_MSF 1.png"
+              alt="EXTRA_04_MSF 1"
+            />
+          </div>
         </div>
         <div className="bg-Neutrals-Grey8 text-Neutrals-White px-4 mx-auto md:max-w-[76rem]">
           <section className="mb-[4rem]">
             <div className="mb-12">
               <div className="mb-8 border-b border-Neutrals-Grey6 w-[6.5rem] mx-auto" />
-              <p className="text-center H4">가엘 튜린 Gaël Turine </p>
+              <p className="text-center H4">아슈피카 라만 Ashfika Rahman</p>
             </div>
             <p className="max-w-[63rem] mx-auto DB1">
-              가엘 튜린은 포켓사진집 포토 포슈 Photo Poche 시리즈의 ‘장벽과 공포
-              Le mur et la peur’, ‘맹목적으로 Aveuglément’ ‘카불에서 스무살 되기
-              Avoir 20 ans à Kaboul’ ‘아이티의 시내 En bas la ville’ 등 다수의
-              모노그래프를 발간한 사진작가이다. 튜린은 유럽과 미국에서 유수의
-              상과 후원을 받았으며, 그의 에세이는 뉴욕 타임스, 르몽드, 뉴스위크
-              재팬 등 여러 매체에 게재됐다.
+              방글라데시 다카 출신의 아슈피카 라흐만은 방글라데시 파트샬라
+              남아시아 언론학교에서 기록사진을 전공했다. 그의 작업은 예술과
+              기록사진의 영역을 넘나드는데, 특히 19세기 판화로부터 영감을 얻어
+              현대 미디어와 결합시킨 독특한 스타일이 돋보인다. 사진이란 매개를
+              통해 사회에 내재된 권력구조와 병폐를 드러낸다.
               <br />
-              <br /> 현재 벨기에 소재 리브르 드 브뤼셀 대학교에서 포토저널리즘을
-              가르치고 있으며, 다양한 국가에서 기록사진 워크숍을 이끌고 있다.
-              튜린은 사진 에이전시 맵스(MAPS)의 창립멤버로, 2017년 창설 이래
-              지금까지 회장직을 맡고 있다.
+              <br />
+              라흐만은 2016년 시작한 장기 프로젝트를 통해 인도와 미얀마 사이
+              방글라데시 외곽 언덕지대의 성폭력 생존자의 현실을 보도해
+              주목받았다. 현재는 방글라데시 북부 군경의 초법적 살인과 납치 등을
+              심도 있게 탐구한 ‘실종자 파일’ 프로젝트를 진행 중이다. 2018년
+              세계보도사진재단의 교육기관 줍 스와트 Joop Swart 마스터클래스에
+              참여했으며, 현재 모교인 방글라데시 남아시아 언론학교의 교수로
+              근무하고 있다.
             </p>
           </section>
           <section className="max-w-[63rem] mx-auto pb-[8rem]">
@@ -110,9 +133,15 @@ export default function AshfikaRahman() {
                 />
                 <p className="mb-4 H4">국경없는의사회는</p>
                 <p className="DB1">
-                  1983년 앙골라에서 첫 활동을 개시했다. 1996년 국경없는의사회는
-                  벤구엘라, 후암보, 쿠안도 쿠반고주 등 앙골라 전역에서 영양실조
-                  프로그램을 운영하며 영양실조 아동에게 의료지원을 제공했다.
+                  1985년 방글라데시에서 첫 의료지원 활동을 시작했다. 현재
+                  국경없는의사회는 로힝야 난민과 방글라데시 지역주민을 대상으로
+                  의료적∙인도적 지원을 제공하고 있으며, 다카 캄란기르차르 지역의
+                  의료 공백에 대응하는 데에도 전력을 다하고 있다.
+                  <br />
+                  <br />
+                  국경없는의사회는 다카에서 여성청소년을 위한 생식 보건 서비스를
+                  제공하고 임신 상담 및 출산을 지원하고 있다. 또한 성폭력 및
+                  가정폭력 피해자에게 의료 및 심리적 지원을 제공한다.
                 </p>
               </div>
               <div>
