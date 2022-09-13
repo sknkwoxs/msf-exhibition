7b0b80ff9e3391a701e17a4d6a5104670a5d65e7
seankim1111 <yongsun199@gmail.com>
2022-09-13 10:28:08 +0900

    add 6번 작가

--- src/pages/photographers/HannahReyesMorales.js
+++ src/pages/photographers/HannahReyesMorales.js
@@ -5,22 +5,26 @@ export default function HannahReyesMorales() {
     <>
       <div className="bg-Neutrals-Grey8 text-Neutrals-White">
         <div className="mb-[4rem] flex justify-center">
-          <img src="/images/GaelTurine/bg.png" alt="bg" />
+          <img src="/images/HannahReyesMorales/bg.png" alt="bg" />
         </div>
         <div className="bg-Neutrals-Grey8 text-Neutrals-White px-4 mx-auto md:max-w-[76rem]">
           <section className="text-center">
             <div>
-              <p className="H3">가엘 튜린 - 앙골라</p>
-              <p className="mb-6 font-bold H3">Gaël Turine - Angola</p>
-              <p className="mb-4 DB1">쿠이토(Kuito), 1996년 3월 2일</p>
-              <p className="mb-[3.375rem] DB3">Mamiya 6, Tri-X 400</p>
+              <p className="H3">해나 레예스 모랄레스 - 필리핀</p>
+              <p className="mb-6 font-bold H3">
+                Hannah Reyes Morales - Philippines
+              </p>
+              <p className="mb-4 DB1">톤도(Tondo), 2017년 7월 31일</p>
+              <p className="mb-[3.375rem] DB3">
+                Sony ILCE-7M2 28mm f3.5 1/3200sec ISO 400
+              </p>
               <p className="text-left max-w-[63rem] mx-auto DB1">
-                앙골라 내전 1975~2002 이 장기화하며 극심한 영양실조로 고통받는
-                아동 환자가 급증했다. 심각한 영양실조 위기가 길어지자 폐렴,
-                설사, 말라리라, 홍역 등으로 아동 사망률이 잇따라 증가했다.
-                1996년, 포토저널리스트 튜린은 앙골라에서 6주를 보내며 무장단체간
-                충돌이 끊이지 않는 비에주 쿠이토의 국경없는의사회 병원을 찾아
-                현장을 기록했다.
+                필리핀 마닐라의 가장 큰 슬럼가 톤도에는 30만 명이 거주하고 있다.
+                ‘마약과의 전쟁’으로 인한 영향을 가장 많이 받은 지역인 톤도의
+                주민들은 많은 위험에 노출된 채 생활하고 있다. 특히 수많은 여성이
+                검사만으로도 예방할 수 있는 자궁경부암으로 고통받고 있다. 2017년
+                모랄레스는 톤도 슬럼가에서 생활하며 이곳의 의료위기에 대해
+                인식을 제고하기 위해 지역주민의 모습을 카메라에 담았다.
               </p>
             </div>
             <div className="my-[6rem] h-0 pb-[55.921%] overflow-hidden relative z-0">
@@ -42,62 +46,85 @@ export default function HannahReyesMorales() {
               </div>
               <div className="mb-[6rem]">
                 <img
-                  src="/images/GaelTurine/GAEL TURINE 02 1.png"
-                  alt="GAEL TURINE 02 1"
+                  src="/images/HannahReyesMorales/HANNAH REYES MORALES 02 1.png"
+                  alt="HANNAH REYES MORALES 02 1"
                 />
-              </div>
-              <p className="mb-4 max-w-[63rem] mx-auto DB1">
-                “쿠이토에 머물던 중 국경없는의사회와 연락이 닿아 함께 작업할 수
-                있었습니다. 국경없는의사회 직원들의 도움으로 현장 상황을 더욱
-                명확히 파악할 수 있었습니다. 위중한 상태로 병원에 실려온
-                부상자들을 봤고, 견디기 힘든 광경을 마주하기도 했습니다. 다행히
-                크게 무겁지 않은 분위기 속에서 환자와 의료진의 모습을 있는
-                그대로 담을 수 있었습니다.”
-              </p>
-              <p className="max-w-[63rem] mx-auto DB1 font-bold">
-                가엘 튜린(2021)
-              </p>
-              <div className="flex gap-8 my-[6rem]">
-                <div>
-                  <img
-                    src="/images/GaelTurine/GAEL TURINE 04 1.png"
-                    alt="GAEL TURINE 04 1"
-                  />
-                </div>
-                <div>
-                  <img
-                    src="/images/GaelTurine/GAEL TURINE 05 1.png"
-                    alt="GAEL TURINE 05 1"
-                  />
-                </div>
               </div>
             </div>
           </section>
-        </div>
-        <div className="mb-[6rem] flex justify-center">
-          <img
-            src="/images/GaelTurine/GAEL TURINE 06 1.png"
-            alt="GAEL TURINE 06 1"
-          />
         </div>
         <div className="bg-Neutrals-Grey8 text-Neutrals-White px-4 mx-auto md:max-w-[76rem]">
           <section className="mb-[4rem]">
             <div className="mb-12">
               <div className="mb-8 border-b border-Neutrals-Grey6 w-[6.5rem] mx-auto" />
-              <p className="text-center H4">가엘 튜린 Gaël Turine </p>
+              <p className="text-center H4">모랄레스의 일기</p>
             </div>
             <p className="max-w-[63rem] mx-auto DB1">
-              가엘 튜린은 포켓사진집 포토 포슈 Photo Poche 시리즈의 ‘장벽과 공포
-              Le mur et la peur’, ‘맹목적으로 Aveuglément’ ‘카불에서 스무살 되기
-              Avoir 20 ans à Kaboul’ ‘아이티의 시내 En bas la ville’ 등 다수의
-              모노그래프를 발간한 사진작가이다. 튜린은 유럽과 미국에서 유수의
-              상과 후원을 받았으며, 그의 에세이는 뉴욕 타임스, 르몽드, 뉴스위크
-              재팬 등 여러 매체에 게재됐다.
+              마닐라의 슬럼가는 지상에 존재하나 거의 지하에 파묻혀 있는
+              느낌이다. 과거에는 슬럼가 주변에 높은 벽을 세워 이곳을 사람들의
+              시야에서 가리려고 했다. 슬럼가는 점점 팽창해 수백만 명의 집이
+              됐고, 동시에 세상에서 가장 인구밀도가 높은 곳이 되었다. 오늘날
+              톤도는 필리핀 정부가 선포한 ‘마약과의 전쟁’의 배경이 되는 곳이다.
+              인권 증진∙보호 단체 휴먼라이츠워치 긴급위기 국장에 따르면
+              톤도에서는 도시 빈민을 겨냥한 “반인도적 범죄”가 자행되고 있다.
               <br />
-              <br /> 현재 벨기에 소재 리브르 드 브뤼셀 대학교에서 포토저널리즘을
-              가르치고 있으며, 다양한 국가에서 기록사진 워크숍을 이끌고 있다.
-              튜린은 사진 에이전시 맵스(MAPS)의 창립멤버로, 2017년 창설 이래
-              지금까지 회장직을 맡고 있다.
+              <br />
+              최근 몇 년 나온 필리핀에 대한 사진들은 대부분 폭력적이고
+              자극적이다. 유혈이 낭자한 모습이 아닌, 아직 세상에 알려지지 않은
+              이야기를 사진에 담을 수 있을까? 높은 장벽 너머 수많은 생명을
+              앗아간 마약과의 전쟁 속에서도 열심히 살아가는 사람들의 모습을
+              보여주고 싶다. - 2017년
+            </p>
+          </section>
+        </div>
+        <div className="mx-auto md:max-w-[76rem] mb-[6rem]">
+          <div className="flex gap-8">
+            <div>
+              <img
+                src="/images/HannahReyesMorales/IMG_20180221_173336 (1) 1.png"
+                alt="IMG_20180221_173336 (1) 1"
+              />
+            </div>
+            <div className="flex flex-col gap-8">
+              <div>
+                <img
+                  src="/images/HannahReyesMorales/11-HRMorales for MSF 1.png"
+                  alt="11-HRMorales for MSF 1"
+                />
+              </div>
+              <div>
+                <img
+                  src="/images/HannahReyesMorales/HRMorales00029 1.png"
+                  alt="HRMorales00029 1"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-Neutrals-Grey8 text-Neutrals-White px-4 mx-auto md:max-w-[76rem]">
+          <section className="mb-[4rem]">
+            <div className="mb-12">
+              <div className="mb-8 border-b border-Neutrals-Grey6 w-[6.5rem] mx-auto" />
+              <p className="text-center H4">
+                해나 레예스 모랄레스 Hannah Reyes Morales
+              </p>
+            </div>
+            <p className="max-w-[63rem] mx-auto DB1">
+              해나 레예스 모랄레스는 필리핀 마닐라에서 태어나 자랐다. 필리핀
+              딜리만 대학교에서 스피치 커뮤니케이션을 전공했다. 모랄레스는
+              불평등, 빈곤과 범죄에 노출된 이들을 조명하는 사진을 찍어왔다.
+              필리핀 해역의 인신매매(뉴욕 타임스), 캄보디아 여성이 겪은
+              전쟁범죄(미국 알자지라), 태풍 이후 성폭력과 인신매매에 더욱 노출된
+              필리핀 여성의 삶을 렌즈에 담았다.
+              <br />
+              <br />
+              모랄레스는 뉴욕 타임스에 연재된 디지털 르포르타주로
+              아시아출판인협회상을 수상했으며, 2019년에는 팀 헤더링턴 상을,
+              2020년에는 뉴욕 국제사진센터가 주관하는 ICP 인피니티 상 ICP
+              Infinity Awards을 수상했다. 모랄레스의 보도사진은 워싱턴 포스트,
+              월스트리트 저널, 타임, 내셔널 지오그래픽 등 다수의 매체에 실렸으며
+              마닐라, 코펜하겐, 치앙마이 등에서 전시됐다. 현재는 내셔널
+              지오그래픽 크리에이티브 소속 작가로 활동하고 있다.
             </p>
           </section>
           <section className="max-w-[63rem] mx-auto pb-[8rem]">
@@ -110,11 +137,14 @@ export default function HannahReyesMorales() {
                 />
                 <p className="mb-4 H4">국경없는의사회는</p>
                 <p className="DB1">
-                  1983년 앙골라에서 첫 활동을 개시했다. 1996년 국경없는의사회는
-                  벤구엘라, 후암보, 쿠안도 쿠반고주 등 앙골라 전역에서 영양실조
-                  프로그램을 운영하며 영양실조 아동에게 의료지원을 제공했다.
+                  1987년 필리핀에서 처음 활동을 시작했고, 2016년부터 수도
+                  마닐라에 위치한 슬럼가 산 안드레스와 톤도에서 종합적인 성∙생식
+                  보건 프로젝트를 운영했다. 2019년에는 여아 및 여성을 위한
+                  프로젝트를 운영하며 가족 계획, 산전후 관리, 성매개 감염 및
+                  자궁경부암 치료를 제공했다.
                 </p>
               </div>
+
               <div>
                 <button className="flex mx-auto mb-6">
                   내 채널에 공유하기
