63412e148124b1e9c36311dd5697b94248473012
seankim1111 <yongsun199@gmail.com>
2022-09-14 18:58:29 +0900

    add react-slick를 사용한 Main page에 mainslide 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { Map } from "react-kakao-maps-sdk";
 
+import Slider from "./components/Slider";
+
 export default function Main() {
   return (
     <>
@@ -47,136 +49,11 @@ export default function Main() {
             </div>
           </section>
           <section className="bg-bg-blur">
-            <div className="py-[6rem] md:max-w-[76rem] mx-auto px-4">
+            <div className="pt-[6rem] md:pb-[10rem] pb-[6rem] md:max-w-[76rem] mx-auto px-4">
               <div className="mb-8">
                 <p className="H4">작가 소개</p>
               </div>
-              <div className="hidden grid-cols-3 grid-rows-1 gap-8 md:grid ">
-                <div>
-                  <Link to="/photographers/GaelTurine">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/gael_thumb.png"
-                        alt="gael_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">가엘 튜린 - 앙골라</p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      Gaël Turine - Angola
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/photographers/AlessandroPenso">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/alessandro_thumb.png"
-                        alt="alessandro_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">
-                      알레산드로 펜소 - 지중해
-                    </p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      Alessandro Penso - Mediterranean Sea
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/photographers/JohnVink">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/john_thumb.png"
-                        alt="john_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">존 빈크 - 온두라스</p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      John Vink - Honduras
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/photographers/DominicNahr">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/dominic_thumb.png"
-                        alt="dominic_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">
-                      도미닉 나흐르 - 남수단
-                    </p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      Dominic Nahr - South Sudan
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/photographers/CedricGerbehaye">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/cedric_thumb.png"
-                        alt="cedric_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">
-                      세드릭 게르베하이 - 콩고민주공화국
-                    </p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      Cédric Gerbehaye - Democratic Republic of the Congo
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/photographers/HannahReyesMorales">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/hannah_thumb.png"
-                        alt="hannah_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">
-                      해나 레예스 모랄레스 - 필리핀
-                    </p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      Hannah Reyes Morales - Philippines
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/photographers/MassimoBerruti">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/massimo_thumb.png"
-                        alt="massimo_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">
-                      마시모 베루티 - 파키스탄
-                    </p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      Massimo Berruti - Pakistan
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link to="/photographers/AshfikaRahman">
-                    <div className="mb-8">
-                      <img
-                        src="/images/mainPageThumbnail/ashfika_thumb.png"
-                        alt="ashfika_thumb"
-                      />
-                    </div>
-                    <p className="mb-2 text-center H5">
-                      아슈피카 라만 - 방글라데시
-                    </p>
-                    <p className="text-center text-Neutrals-Grey5 DB2">
-                      Ashfika Rahman - Bangladesh
-                    </p>
-                  </Link>
-                </div>
-              </div>
+              <Slider />
               <div className="grid grid-cols-1 grid-rows-1 gap-8 md:hidden ">
                 <div>
                   <div className="h-0 pb-[75%] overflow-hidden relative z-0 mb-8">
