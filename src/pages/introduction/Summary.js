6614785575675a70b8fd4797aa1cf77a5054a4b6
seankim1111 <yongsun199@gmail.com>
2022-09-19 13:38:12 +0900

    style Summary page background image 추가

--- src/pages/introduction/Summary.js
+++ src/pages/introduction/Summary.js
@@ -10,44 +10,49 @@ export default function Summary() {
             alt="SD-default"
           />
         </div>
-        <div className="absolute w-full">
+        {/* <div className="absolute w-full">
           <img
-            className="object-cover w-full max-h-[45.5rem]"
+            className="w-full "
             src="/images/Asset 1@3x 2.png"
             alt="Asset 1@3x 2"
           />
-        </div>
-        <div className="mx-auto md:max-w-[76rem] px-4 DB1 text-Neutrals-Grey8">
-          <p className="md:my-[6rem] my-[3rem]">
-            ‘치료’와 ‘증언’은 국경없는의사회가 존재하는 두 가지 이유입니다.
-            국경없는의사회는 지난 50년동안 전 세계의 인도적 위기 현장에서 생존을
-            위협받는 환자에게 의료지원을 제공하는 동시에 환자가 처한 현실을
-            세상에 알리기 위해 노력해왔습니다. 우리가 현장에서 목격한 것을
-            ‘증언’하는 것이 현실을 바꿀 방법이 될 수 있다고 믿기 때문입니다.
-            <br />
-            <br />
-            사진은 사실을 있는 그대로 전하는 데 중요한 도구입니다.
-            국경없는의사회 한국 사무소는 설립 10주년을 기념하기 위해 개최되는
-            <span className="font-bold">MORE THAN A PICTURE</span> 사진전을 통해
-            국경없는의사회가 50년간 걸어온 길 위, 우리가 기억해야 할 순간들을
-            포착한 사진들을 소개합니다.
-            <br />
-            <br />
-            세계적인 사진 에이전시 맵스 MAPS IMAGES 소속 보도사진작가 8인이
-            남수단, 방글라데시, 앙골라, 온두라스, 지중해, 콩고민주공화국,
-            파키스탄, 필리핀. 8개 국가에서 포착한 인도적 위기 현장을 통해
-            국경없는의사회의 생명을 살리는 발걸음에 함께 해주시길 바랍니다.
-            <br />
-            <br />
-          </p>
-          <p className="mb-6 text-Brand-Primary H5">맵스 이미지(MAPS IMAGES)</p>
-          <p className="md:mb-[6rem] mb-[3rem]">
-            2017년 9월 설립된 보도사진 작가 그룹으로 세계적인 사진작가인 도미닉
-            나흐르, 가엘 튜린, 존 빙크 등 16명의 소속 작가들이 활동하고 있다.
-            빠르게 변화하는 사회의 모습과 단조로운 일상을 새로운 시각으로
-            담아내고 있으며, 국제 사회가 직면하고 있는 인도적 위기 현장을 포착해
-            세계 곳곳에 알리고 있다.
-          </p>
+        </div> */}
+        <div className="px-4 bg-center bg-no-repeat bg-cover DB1 text-Neutrals-Grey8 bg-bg-introduction">
+          <div className="mx-auto md:max-w-[76rem]">
+            <p className="md:py-[6rem] py-[3rem]">
+              ‘치료’와 ‘증언’은 국경없는의사회가 존재하는 두 가지 이유입니다.
+              국경없는의사회는 지난 50년동안 전 세계의 인도적 위기 현장에서
+              생존을 위협받는 환자에게 의료지원을 제공하는 동시에 환자가 처한
+              현실을 세상에 알리기 위해 노력해왔습니다. 우리가 현장에서 목격한
+              것을 ‘증언’하는 것이 현실을 바꿀 방법이 될 수 있다고 믿기
+              때문입니다.
+              <br />
+              <br />
+              사진은 사실을 있는 그대로 전하는 데 중요한 도구입니다.
+              국경없는의사회 한국 사무소는 설립 10주년을 기념하기 위해 개최되는
+              <span className="font-bold">MORE THAN A PICTURE</span> 사진전을
+              통해 국경없는의사회가 50년간 걸어온 길 위, 우리가 기억해야 할
+              순간들을 포착한 사진들을 소개합니다.
+              <br />
+              <br />
+              세계적인 사진 에이전시 맵스 MAPS IMAGES 소속 보도사진작가 8인이
+              남수단, 방글라데시, 앙골라, 온두라스, 지중해, 콩고민주공화국,
+              파키스탄, 필리핀. 8개 국가에서 포착한 인도적 위기 현장을 통해
+              국경없는의사회의 생명을 살리는 발걸음에 함께 해주시길 바랍니다.
+              <br />
+              <br />
+            </p>
+            <p className="pb-6 text-Brand-Primary H5">
+              맵스 이미지(MAPS IMAGES)
+            </p>
+            <p className="md:pb-[6rem] pb-[3rem]">
+              2017년 9월 설립된 보도사진 작가 그룹으로 세계적인 사진작가인
+              도미닉 나흐르, 가엘 튜린, 존 빙크 등 16명의 소속 작가들이 활동하고
+              있다. 빠르게 변화하는 사회의 모습과 단조로운 일상을 새로운
+              시각으로 담아내고 있으며, 국제 사회가 직면하고 있는 인도적 위기
+              현장을 포착해 세계 곳곳에 알리고 있다.
+            </p>
+          </div>
         </div>
       </div>
     </>
