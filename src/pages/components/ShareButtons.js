927d25b8ac3de02e96ea948ebfc30ee60aa3362d
seankim1111 <yongsun199@gmail.com>
2022-09-20 11:45:38 +0900

    .env file 생성 및 app key 등록

--- src/pages/components/ShareButtons.js
+++ src/pages/components/ShareButtons.js
@@ -4,33 +4,6 @@ import CopyToClipboard from "react-copy-to-clipboard";
 const ShareButtons = () => {
   const currentUrl = window.location.href;
 
-  useEffect(() => {
-    Kakao.init("Kakao-Dev javascript 키값");
-  }, []);
-
-  const shareKakao = () => {
-    Kakao.Link.sendDefault({
-      objectType: "feed",
-      content: {
-        title: themeData.title,
-        description: "내용",
-        imageUrl: themeData.titleImage,
-        link: {
-          mobileWebUrl: "모바일 url",
-          androidExecParams: "test",
-        },
-      },
-      buttons: [
-        {
-          title: "웹으로 이동",
-          link: {
-            mobileWebUrl: "공유할 url",
-          },
-        },
-      ],
-    });
-  };
-
   return (
     <>
       <CopyToClipboard text={currentUrl}>
