a4412525322de946f8d75c853145224cfe96d3f6
seankim1111 <yongsun199@gmail.com>
2022-09-21 11:34:00 +0900

    add router active class menu에 css 추가

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { introduction, photographers } from "./components/SubmenuItems";
 import Collapsible from "react-collapsible";
-import { HashLink } from "react-router-hash-link";
+import { HashLink, NavHashLink } from "react-router-hash-link";
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -24,6 +24,8 @@ const Header = () => {
     window.addEventListener("scroll", updateScroll);
   });
 
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <>
       <header className="fixed top-0 left-0 right-0 z-10 text-Neutrals-White bg-Neutrals-Grey9 md:bg-inherit">
@@ -31,7 +33,10 @@ const Header = () => {
           id="headerWrap"
           className={scrollPosition < 1 ? "bg_transparent" : "bg_black"}
         >
-          <div className="hidden md:flex justifㄹy-between px-4 mx-auto max-w-[76rem] relative">
+          <div
+            id="headerContents"
+            className="hidden md:flex justifiy-between px-4 mx-auto max-w-[76rem] relative"
+          >
             <a href="/">
               <img
                 className="py-[1.75rem]"
@@ -39,10 +44,20 @@ const Header = () => {
                 alt="logo"
               />
             </a>
-            <div className="absolute right-0 top-10">
-              <ul className="flex text-[1.25rem] leading-[1.5rem] font-bold header_menu">
+            <div className="absolute right-0 h-full top-10">
+              <ul
+                className="flex text-[1.25rem] leading-[1.5rem] font-bold header_menu"
+                onMouseOver={() => setIsHover(true)}
+                onMouseOut={() => setIsHover(false)}
+              >
                 <li className="mx-[2rem] relative inline-block py-2 expanded">
-                  <HashLink to="/introduction/Summary#">사진전 소개</HashLink>
+                  <NavHashLink
+                    to="/introduction/Summary#"
+                    activeClassName="selected"
+                    activeStyle={{ color: "red" }}
+                  >
+                    사진전 소개
+                  </NavHashLink>
                   <ul className="absolute top-[7.5rem] text-center whitespace-nowrap flex flex-col gap-4 left-[50%] -translate-x-[50%] header_submenu">
                     {introduction.map((introduction, index) => {
                       return (
@@ -56,7 +71,13 @@ const Header = () => {
                   </ul>
                 </li>
                 <li className="mx-[2rem] relative inline-block py-2 expanded">
-                  <HashLink to="/photographers/GaelTurine#">작가 소개</HashLink>
+                  <NavHashLink
+                    to="/photographers/GaelTurine#"
+                    activeClassName="selected"
+                    activeStyle={{ color: "red" }}
+                  >
+                    작가 소개
+                  </NavHashLink>
                   <ul className="absolute top-[7.5rem] text-center whitespace-nowrap flex flex-col gap-4 left-[50%] -translate-x-[50%] header_submenu">
                     {photographers.map((photographers, index) => {
                       return (
@@ -70,10 +91,22 @@ const Header = () => {
                   </ul>
                 </li>
                 <li className="mx-[2rem] relative inline-block py-2">
-                  <HashLink to="/Participate#">참여 방법</HashLink>
+                  <NavHashLink
+                    to="/Participate#"
+                    activeClassName="selected"
+                    activeStyle={{ color: "red" }}
+                  >
+                    참여 방법
+                  </NavHashLink>
                 </li>
                 <li className="mx-[2rem] relative inline-block py-2">
-                  <HashLink to="/Address#">오시는길</HashLink>
+                  <NavHashLink
+                    to="/Address#"
+                    activeClassName="selected"
+                    activeStyle={{ color: "red" }}
+                  >
+                    오시는길
+                  </NavHashLink>
                 </li>
               </ul>
             </div>
